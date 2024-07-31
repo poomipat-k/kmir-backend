dccaa14c40396ce5cf2e62e83c2913190c43b486
poomipat-k <k.poomipat@gmail.com>
2024-07-31 21:56:45 +0700

    feature/admin update seed data

--- migrations/20240725172511_add_admin_note_table.sql
+++ migrations/20240725172511_add_admin_note_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE admin_note (
   note TEXT
 );
 
-INSERT INTO admin_note (note) VALUES ('<p>Korem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nil, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.</p>');
+INSERT INTO admin_note (note) VALUES ('<p>-</p>');
 
 -- +goose Down
 DROP TABLE admin_note;
